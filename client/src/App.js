d43d3c901e8db3f125b2db5398ddb5d376c5e6df
Natasha Beddow <beddow90@gmail.com>
2022-09-03 13:32:05 +0100

    card now renders

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import EventCard from "./components/EventCard";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <EventCard />
     </div>
   );
 }
--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -5,7 +5,7 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 
-export default function ActionAreaCard() {
+export default function EventCard(props) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
@@ -17,11 +17,10 @@ export default function ActionAreaCard() {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            Lizard
+            {props.title}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            {props.description}
           </Typography>
         </CardContent>
       </CardActionArea>
