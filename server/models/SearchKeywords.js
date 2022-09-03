594099c68b5365e4c935b5fdea642cf889289379
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-03 12:54:51 +0100

    example search

--- server/models/SearchKeywords.js
+++ server/models/SearchKeywords.js
@@ -1,6 +1,28 @@
 const { Schema, model } = require('mongoose');
 
-const searchKeywordsSchema = new Schema({});
+const searchKeywordsSchema = new Schema({
+  reviewId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  reviewText: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  eventId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Event',
+  },
+});
 
 const SearchKeywords = model('SearchKeywords', searchKeywordsSchema);
 
