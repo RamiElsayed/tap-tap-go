4df804a4f7d679201ce66f1eb94e8954add43d2d
frsargua <frsargua@gmail.com>
2022-09-03 20:39:47 +0100

    Created check again area

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import PrimarySearchAppBar from "./components/navbar/index.js";
 import Hero from "./components/Hero/index.js";
+import RecentlySeen from "./components/RecentlySeen/index.js";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <PrimarySearchAppBar></PrimarySearchAppBar>
       <Hero></Hero>
+      <RecentlySeen></RecentlySeen>
     </>
   );
 }
