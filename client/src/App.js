aa57179de8d6311afe21b4687f95eb756d03cf1a
frsargua <frsargua@gmail.com>
2022-09-03 19:42:00 +0100

    Create hero banner with autocomplete

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import PrimarySearchAppBar from "./components/navbar/index.js";
+import Hero from "./components/Hero/index.js";
 import "./App.css";
 
 function App() {
-  return <PrimarySearchAppBar></PrimarySearchAppBar>;
+  return (
+    <>
+      <PrimarySearchAppBar></PrimarySearchAppBar>
+      <Hero></Hero>
+    </>
+  );
 }
 
 export default App;
