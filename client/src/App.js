dcfef404f2f950a5504ec701e9069b3eb6bd4a30
frsargua <frsargua@gmail.com>
2022-09-04 09:51:25 +0100

    Temporary change of imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,10 @@
-import PrimarySearchAppBar from "./components/navbar/index.js";
-import Hero from "./components/Hero/index.js";
-import RecentlySeen from "./components/RecentlySeen/index.js";
-import "./App.css";
+import Landing from "./Pages/Landing";
+import Profile from "./Pages/Profile";
 
 function App() {
   return (
     <>
-      <PrimarySearchAppBar></PrimarySearchAppBar>
-      <Hero></Hero>
-      <RecentlySeen></RecentlySeen>
+      <Profile></Profile>
     </>
   );
 }
