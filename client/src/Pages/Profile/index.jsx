ab4bc535ad33a016f9e103ee068ff2d772c3b146
frsargua <frsargua@gmail.com>
2022-09-04 10:34:15 +0100

    Layout of Profile page

--- client/src/Pages/Profile/index.jsx
+++ client/src/Pages/Profile/index.jsx
@@ -1,14 +1,31 @@
+import { Grid, ListItem } from "@mui/material";
+import { Container } from "@mui/system";
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
+import Options from "./Options";
+import Banner from "./Banner";
 
 function Profile() {
   const [open, setOpen] = useState(false);
 
   return (
-    <Sidebar
-      isOpenSidebar={open}
-      onCloseSidebar={() => setOpen(false)}
-    ></Sidebar>
+    <Container maxWidth="xl">
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Banner></Banner>
+        </Grid>
+        <Grid item xs={12}>
+          <Options></Options>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <ListItem>Profile</ListItem>
+          <ListItem>Sidebar with info</ListItem>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          Posts
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
