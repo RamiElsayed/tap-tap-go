3ca3f19107c8503c2e3865466ac5d69f0a9387e1
frsargua <frsargua@gmail.com>
2022-09-04 12:00:33 +0100

    create event frame

---
+++ client/src/Pages/Profile/CreteEvent.jsx
@@ -0,0 +1,17 @@
+import { Card, CardContent, Paper, Typography } from "@mui/material";
+import { Stack } from "@mui/system";
+
+function PostingBoard() {
+  return (
+    <Card sx={{ marginBottom: "1rem" }}>
+      <CardContent>
+        <Stack>
+          <Typography variant="h6">Create your own event</Typography>
+          <Typography variant="body1">Create event</Typography>
+        </Stack>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default PostingBoard;
