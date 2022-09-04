1921e3e908ed5b082291dbe2b10e62ad0d67f46e
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-04 19:41:39 +0100

    remove rating from seed index

--- server/server.js
+++ server/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const connectToDatabase = require('./config/connection');
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
+// const typeDefs = require('./typeDefs');
+// const resolvers = require('./resolvers');
 const { authMiddleware } = require('./context/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -27,21 +27,21 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/'));
 });
 
-const init = async (typeDefs, resolvers) => {
+const init = async () => {
   try {
     await connectToDatabase();
-    await server.start();
-    server.applyMiddleware({ app });
+    // await server.start();
+    // server.applyMiddleware({ app });
 
     app.listen(PORT, () => {
-      console.log(`API server listening on http://localhost:${PORT}`),
-        console.log(
-          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`,
-        );
+      console.log(`API server listening on http://localhost:${PORT}`);
+      // console.log(
+      //   `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`,
+      // );
     });
   } catch (err) {
     console.log(`Failed to initiate API server || ${err.message}`);
   }
 };
 
-init(typeDefs, resolvers);
+init();
