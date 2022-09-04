c29ee88e9e1398d7c5b41fccffe1e40b35d65f04
frsargua <frsargua@gmail.com>
2022-09-04 22:45:08 +0100

    added cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,20 @@
 import PrimarySearchAppBar from "./components/navbar/index.js";
 import Hero from "./components/Hero/index.js";
 import RecentlySeen from "./components/RecentlySeen/index.js";
-import EventCard from "./components/Card/EventCard";
+import EventCard from "./components/Cards/EventCard";
+import Cards from "./components/Cards/index.js";
 import "./App.css";
 
 import Profile from "./Pages/Profile";
 import NavBar from "./components/navbar/index";
+import { Card } from "@mui/material";
 function App() {
-  const title = "Salsa with Fabian Sarango";
-  const price = "From £15 per person";
-  const value = 4;
-  const nRatings = 254;
-
   return (
     <>
       <PrimarySearchAppBar></PrimarySearchAppBar>
       <Hero></Hero>
-      <RecentlySeen></RecentlySeen>
-      <EventCard
-        title={title}
-        price={price}
-        value={value}
-        nRatings={nRatings}
-      ></EventCard>
+      {/* <RecentlySeen></RecentlySeen> */}
+      <Cards />
     </>
   );
 }
