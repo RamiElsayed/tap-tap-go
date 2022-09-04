7cb0d84501b1188dd6a410164af08d7acae829ef
frsargua <frsargua@gmail.com>
2022-09-04 16:10:50 +0100

    Made options a component

--- client/src/Pages/Profile/Options.jsx
+++ client/src/Pages/Profile/Options.jsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, Paper, Stack } from "@mui/material";
+import { Box, Card, CardContent, Paper, Stack } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import { Container } from "@mui/system";
 import React, { useState } from "react";
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -8,25 +7,33 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
+  paddingRight: theme.spacing(3),
   textAlign: "center",
   color: theme.palette.error.dark,
   fontWeight: 500,
+  whiteSpace: "nowrap",
 }));
 
 function Options() {
   const [open, setOpen] = useState(false);
 
   return (
-    <Card elevation={1}>
-      <Stack direction="row" spacing={5}>
-        <Item>Activities</Item>
-        <Item>Tickets</Item>
-        <Item>Bookmarks</Item>
-        <Item>Own events</Item>
-        <Item>Reviews</Item>
-        <Item>Manage</Item>
-      </Stack>
-    </Card>
+    <Box
+      overflow={true}
+      sx={{
+        display: "flex",
+        overflow: "auto",
+        paddingLeft: "2rem",
+        paddingBottom: "0.5rem",
+      }}
+    >
+      <Item>Activities</Item>
+      <Item>Tickets</Item>
+      <Item>Bookmarks</Item>
+      <Item>Own events</Item>
+      <Item>Reviews</Item>
+      <Item>Manage</Item>
+    </Box>
   );
 }
 
