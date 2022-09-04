5c9141e09e12cfaf5cff82f76bd0f32edce646f2
frsargua <frsargua@gmail.com>
2022-09-04 17:32:33 +0100

    Added navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import Landing from "./Pages/Landing";
 import Profile from "./Pages/Profile";
-
+import NavBar from "./components/navbar/index";
 function App() {
   return (
     <>
+      <NavBar></NavBar>
       <Profile></Profile>
     </>
   );
