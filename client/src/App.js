26343554cc3256140577ae56fb84d17400e795cd
frsargua <frsargua@gmail.com>
2022-09-05 12:57:52 +0100

    Use only pages code instead of components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,12 @@
-import PrimarySearchAppBar from "./components/navbar/index.js";
-import Hero from "./components/Hero/index.js";
-import RecentlySeen from "./components/RecentlySeen/index.js";
-import EventCard from "./components/Cards/EventCard";
-import Cards from "./components/Cards/index.js";
 import "./App.css";
 
 import Profile from "./Pages/Profile";
-import NavBar from "./components/navbar/index";
-import { Card } from "@mui/material";
+import Landing from "./Pages/Landing";
+
 function App() {
   return (
     <>
-      <PrimarySearchAppBar></PrimarySearchAppBar>
-      <Hero></Hero>
-      {/* <RecentlySeen></RecentlySeen> */}
-      <Cards />
+      <Landing />
     </>
   );
 }
