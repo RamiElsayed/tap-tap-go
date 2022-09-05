b84e865d30ae99ccdf5d7359fb22391d07fc41d4
frsargua <frsargua@gmail.com>
2022-09-05 14:59:48 +0100

    Created eventsPage

---
+++ client/src/Pages/EventPage/index.js
@@ -0,0 +1,15 @@
+import ReviewCard from "../../components/ReviewCard/index";
+
+const cardData = {
+  title: "Best Place to learn a new skill!",
+  author: "Fabian Sarango",
+  description:
+    "I can suggest this is the best website for finding the next place to go! I will recommend it to my friends",
+  value: 4,
+};
+
+function EventPage() {
+  return <ReviewCard cardData={cardData} />;
+}
+
+export default EventPage;
