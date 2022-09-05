503145c4718f60da65a6a5d35ba4c49b3ae98f45
frsargua <frsargua@gmail.com>
2022-09-05 15:52:13 +0100

    Review Form. Non responsive and non functional

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -1,5 +1,77 @@
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button, CardContent, Toolbar, Typography } from "@mui/material";
+import Rating from "@mui/material/Rating";
+import React, { useState } from "react";
+import { Stack } from "@mui/system";
+
 function ReviewForm() {
-    return (  );
+  const [newReview, setReview] = useState({
+    rating: 0,
+    headline: "",
+    review: "",
+  });
+
+  const updateReview = (event) => {
+    let { name, value } = event.target;
+    setReview((prev) => {
+      return {
+        prev,
+        [name]: value,
+      };
+    });
+    console.log(newReview);
+  };
+
+  const printState = (event) => {
+    event.preventdefault();
+    console.log(newReview);
+  };
+
+  return (
+    <Card sx={{ maxWidth: "600px" }}>
+      <CardContent>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "400px",
+            marginX: "auto",
+          }}
+        >
+          <form onChange={updateReview}>
+            <Rating name="rating" value={null} size="large" />
+
+            <Typography variant="h6" fontWeight="600">
+              Add a headline
+            </Typography>
+            <TextField
+              fullWidth
+              name="headline"
+              placeholder="what is most important to share?"
+              id="fullWidth"
+              size="medium"
+              margin="dense"
+            />
+            <Typography variant="h6" fontWeight="600">
+              Write your review
+            </Typography>
+            <TextField
+              fullWidth
+              name="review"
+              placeholder="What id you enjoy and disliked from the event?"
+              id="fullWidth"
+              size="medium"
+              sx={{ marginBottom: "1rem" }}
+            />
+            <Button type="submit" variant="outlined" onSubmit={printState}>
+              Submit
+            </Button>
+          </form>
+        </Box>
+      </CardContent>
+    </Card>
+  );
 }
 
 export default ReviewForm;
