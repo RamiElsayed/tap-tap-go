6a4952218aa1c1ebe07ecf57bf92abe7796ec094
frsargua <frsargua@gmail.com>
2022-09-05 11:15:19 +0100

    removed container and added section margin at the bottom

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -6,26 +6,24 @@ import { Typography } from "@mui/material";
 
 const Hero = () => {
   return (
-    <Container maxWidth="xl">
-      <Grid container spacing={2}>
-        <Grid
-          item
-          justifyContent="center"
-          xs={12}
-          md={8}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            marginTop: { xs: "3rem", md: "0rem" },
-          }}
-        >
-          <Search></Search>
-        </Grid>
-        <Grid item xs={0} md={4}>
-          <QuiltedImageList></QuiltedImageList>
-        </Grid>
+    <Grid container spacing={2} className="section__block-2">
+      <Grid
+        item
+        justifyContent="center"
+        xs={12}
+        md={8}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          marginTop: { xs: "3rem", md: "0rem" },
+        }}
+      >
+        <Search></Search>
       </Grid>
-    </Container>
+      <Grid item xs={0} md={4}>
+        <QuiltedImageList></QuiltedImageList>
+      </Grid>
+    </Grid>
   );
 };
 export default Hero;
