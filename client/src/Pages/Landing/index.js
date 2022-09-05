e322e9cdacd982c53e147372b895ee1d7c586513
frsargua <frsargua@gmail.com>
2022-09-05 11:15:44 +0100

    Landing page layout

--- client/src/Pages/Landing/index.js
+++ client/src/Pages/Landing/index.js
@@ -1,13 +1,25 @@
 import PrimarySearchAppBar from "../../components/navbar/index.js";
 import Hero from "../../components/Hero/index.js";
 import RecentlySeen from "../../components/RecentlySeen/index.js";
+import Cards from "../../components/Cards/index";
+import { Typography } from "@mui/material";
+import TopSellers from "../../components/TopSellers/index.js";
+import { Container } from "@mui/system";
 
 export default function Landing() {
   return (
     <>
-      <PrimarySearchAppBar></PrimarySearchAppBar>
-      <Hero></Hero>
-      <RecentlySeen></RecentlySeen>
+      <Container maxWidth="xl">
+        <PrimarySearchAppBar></PrimarySearchAppBar>
+        <Hero></Hero>
+        <RecentlySeen></RecentlySeen>
+        <Typography variant="h4" textAlign="center" gutterBottom>
+          Want to learn to dance?? Check in your area
+        </Typography>
+        <Cards />
+        <TopSellers></TopSellers>
+        <Cards />
+      </Container>
     </>
   );
 }
