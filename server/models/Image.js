e5c5dfa691b47151a12b39c66afc5667fc6c7213
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-06 17:07:07 +0100

    added image model

---
+++ server/models/Image.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const imageSchema = new Schema({
+  imageLink: {
+    type: String,
+    required: true,
+  },
+});
+
+const Image = model('Image', imageSchema);
+
+module.exports = Image;
--- server/models/index.js
+++ server/models/index.js
@@ -3,5 +3,6 @@ const Event = require('./Event');
 const Review = require('./Review');
 const Tag = require('./Tag');
 const Location = require('./Location');
+const Image = require('./Image');
 
-module.exports = { User, Event, Review, Tag, Location };
+module.exports = { User, Event, Review, Tag, Location, Image };
