d086b75426909a617ba15267f30cc36c554db83a
Rami Elsayed <ramielsayed123@outlook.com>
2022-09-06 20:54:35 +0100

    graphql scalars package

---
+++ server/typeDefs/index.js
@@ -0,0 +1,35 @@
+const { gql } = require("apollo-server");
+
+const typeDefs = gql`
+  scalar Date
+  type User {
+    _id: ID!
+    firstName: String!
+    flastName: String!
+    number: Int!
+    email: String!
+    events: [Event]!
+  }
+  type Event {
+    _id: ID!
+    username: String!
+    eventName: String!
+    location: String!
+    description: String
+    date: Date
+    price: Int
+    ageGroup: String!
+    images: [String]!
+    tags: [String]
+    reviews: [String]
+    attendees: Int!
+    maxAttendees: Int!
+  }
+  type Query {
+    users: [User]
+    evetnts: [Event]
+    event(eventId: ID!): Event
+  }
+`;
+
+module.exports = typeDefs;
