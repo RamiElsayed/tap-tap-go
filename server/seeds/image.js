ccd4c2c539b3c36617c9461d2a26dce9753a7fd1
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-06 18:23:34 +0100

    finished image seeding file

--- server/seeds/image.js
+++ server/seeds/image.js
@@ -1,54 +1,40 @@
 const { Event, Image } = require('../models');
 const { faker } = require('@faker-js/faker/locale/en_GB');
 
-const generateLocations = async () => {
+const generateImages = async () => {
   const events = await Event.find({});
 
   for (let i = 0; i < events.length; i++) {
-    const { _id } = events[i];
+    const { _id: eventId } = events[i];
 
-    const buildingNumber = faker.address.buildingNumber();
-    const streetName = faker.address.streetName();
-    const cityName = faker.address.city();
-    const county = faker.address.county();
-    const latitude = faker.address.latitude(50.10319, 60.15456, 5);
-    const longitude = faker.address.longitude(-7.64133, 1.75159, 5);
-    const state = faker.address.state();
-    const postcode = faker.address.zipCodeByState(state);
-    const eventId = _id;
+    const numberOfImages = Math.floor(Math.random() * 5);
 
-    const location = {
-      buildingNumber,
-      streetName,
-      cityName,
-      county,
-      latitude,
-      longitude,
-      state,
-      postcode,
-      eventId,
-    };
+    for (let j = 0; j < numberofImages; j++) {
+      const imageLink = faker.image.city(200, 300);
 
-    const createdLocation = await Location.create(Location);
+      const image = { imageLink, eventId };
 
-    const { _id: locationId } = createdLocation;
+      const createdImage = await Image.create(image);
 
-    await Event.findByIdAndUpdate(eventId, {
-      $set: {
-        location: locationId,
-      },
-    });
+      const { _id: imageId } = createdImage;
+
+      await Event.findByIdAndUpdate(eventId, {
+        $push: {
+          images: imageId,
+        },
+      });
+    }
   }
 };
 
-const seedLocations = async () => {
+const seedImages = async () => {
   try {
-    const reviews = await generateLocations();
+    const images = await generateImages();
 
-    console.log('Successfully seeded locations data.');
+    console.log('Successfully seeded Images data.');
   } catch (err) {
-    console.log(`Failed to seed locations data || ${err.message}`);
+    console.log(`Failed to seed Images data || ${err.message}`);
   }
 };
 
-module.exports = seedLocations;
+module.exports = seedImages;
