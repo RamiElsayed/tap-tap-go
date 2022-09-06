ff50138fc655e8f41bc89fd8b8151fd83aad1187
frsargua <frsargua@gmail.com>
2022-09-06 15:21:59 +0100

    Moved title and theme to the parent component

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -2,27 +2,49 @@ import { Grid } from "@mui/material";
 import React from "react";
 import QuiltedImageList from "./sub-components/ImagesColumn";
 import Search from "./sub-components/Search";
+import { Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+let theme = createTheme({
+  // ...
+});
+
+theme = createTheme(theme, {
+  typography: {
+    h1: {
+      fontSize: 73,
+      [theme.breakpoints.only("xs")]: {
+        fontSize: 55,
+      },
+    },
+  },
+});
 
 const Hero = () => {
   return (
-    <Grid container spacing={2} className="section__block-5">
-      <Grid
-        item
-        justifyContent="center"
-        xs={12}
-        md={8}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          marginTop: { xs: "3rem", md: "0rem" },
-        }}
-      >
-        <Search></Search>
+    <ThemeProvider theme={theme}>
+      <Grid container spacing={2} className="section__block-5">
+        <Grid
+          item
+          justifyContent="center"
+          xs={12}
+          md={8}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            marginTop: { xs: "3rem", md: "0rem" },
+          }}
+        >
+          <Typography variant="h1" gutterBottom textAlign="center">
+            Hobbies everywhere!
+          </Typography>
+          s<Search></Search>
+        </Grid>
+        <Grid item xs={0} md={4}>
+          <QuiltedImageList></QuiltedImageList>
+        </Grid>
       </Grid>
-      <Grid item xs={0} md={4}>
-        <QuiltedImageList></QuiltedImageList>
-      </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 };
 export default Hero;
