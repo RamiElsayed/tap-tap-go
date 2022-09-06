14341fe0413489e3efefb42d3efebc3ee97137c2
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-06 15:46:01 +0100

    initial location schema created + added to index

---
+++ server/models/Location.js
@@ -0,0 +1,63 @@
+const { Schema, model } = require('mongoose');
+
+const locationSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  eventName: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+    get: formatDate,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  ageGroup: {
+    type: String,
+    required: true,
+  },
+  images: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  tags: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Tag',
+    },
+  ],
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Review',
+    },
+  ],
+  attendees: {
+    type: Number,
+    required: true,
+  },
+  maxAttendees: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Location = model('Location', locationSchema);
+
+module.exports = Location;
--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,7 @@
-const User = require("./User");
-const Event = require("./Event");
-const Review = require("./Review");
-const Tag = require("./Tag");
+const User = require('./User');
+const Event = require('./Event');
+const Review = require('./Review');
+const Tag = require('./Tag');
+const Location = require('./Location');
 
-module.exports = { User, Event, Review, Tag };
+module.exports = { User, Event, Review, Tag, Location };
