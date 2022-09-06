ac5a1fc71eae8e8a8dd8544ed473f318c5341c6c
frsargua <frsargua@gmail.com>
2022-09-06 16:53:47 +0100

    reusable search bar

--- client/src/components/Hero/sub-components/Search.jsx
+++ client/src/components/Hero/sub-components/Search.jsx
@@ -2,44 +2,41 @@ import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-import { top100Films } from "./fakeData";
 
-export default function Search() {
+export default function Search({ category, inputLabel, position }) {
   const [open, setOpen] = React.useState(false);
 
   let searchSuggestionLimit = 5;
 
   return (
-    <Stack sx={{ width: { xs: "100%", md: "60%" }, marginX: "auto" }}>
-      <Autocomplete
-        // freeSolo
-        open={open}
-        limitTags={4}
-        options={top100Films.filter((option, i) => {
-          if (i < searchSuggestionLimit) {
-            return option;
-          }
-        })}
-        getOptionLabel={(option) => option.title}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search by location or event"
-            InputProps={{
-              ...params.InputProps,
-              type: "search",
-            }}
-          />
-        )}
-        onInputChange={(_, value) => {
-          if (value.length === 0) {
-            if (open) setOpen(false);
-          } else {
-            if (!open) setOpen(true);
-          }
-        }}
-        onClose={() => setOpen(false)}
-      />
-    </Stack>
+    <Autocomplete
+      sx={{ width: { xs: "100%", md: "60%" }, margin: position }}
+      open={open}
+      limitTags={4}
+      options={category.filter((option, i) => {
+        if (i < searchSuggestionLimit) {
+          return option;
+        }
+      })}
+      getOptionLabel={(option) => option.title}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={inputLabel}
+          InputProps={{
+            ...params.InputProps,
+            type: "search",
+          }}
+        />
+      )}
+      onInputChange={(_, value) => {
+        if (value.length === 0) {
+          if (open) setOpen(false);
+        } else {
+          if (!open) setOpen(true);
+        }
+      }}
+      onClose={() => setOpen(false)}
+    />
   );
 }
