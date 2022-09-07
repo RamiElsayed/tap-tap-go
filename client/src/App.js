87d5c407e09a94592182f24ff9b2cc61155785b0
Amina Hayat <hayat.amina@hotmail.com>
2022-09-07 14:09:13 +0100

    updatesd app changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-
+import "./login";
+import SignIn from "./login";
 import Profile from "./Pages/Profile";
 import Landing from "./Pages/Landing";
 import EventPage from "./Pages/EventPage";
@@ -7,7 +8,7 @@ import EventPage from "./Pages/EventPage";
 function App() {
   return (
     <>
-      <EventPage />
+      <SignIn />
     </>
   );
 }
