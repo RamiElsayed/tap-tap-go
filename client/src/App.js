2837b063391c1daa408e7e3a61bf44a003426d81
frsargua <frsargua@gmail.com>
2022-09-07 13:52:49 +0100

    Created routing between pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import "./App.css";
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import SignIn from "./login";
 import Profile from "./Pages/Profile";
+import ResponsiveAppBar from "./components/navbar";
 import Landing from "./Pages/Landing";
 import EventPage from "./Pages/EventPage";
-import EventForm from "./components/Eventform/index";
+import { Container } from "@mui/system";
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <EventForm />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Container maxWidth="xl">
+          <ResponsiveAppBar />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route>
+              <Route path="/user/:usedId" element={<Profile />} />
+            </Route>
+          </Routes>
+        </Container>
+      </Router>
+    </ApolloProvider>
   );
 }
 
