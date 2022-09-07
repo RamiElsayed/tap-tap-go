37f09f590b79da7f41e7dd4a7b3aa4486fe67970
frsargua <frsargua@gmail.com>
2022-09-07 17:39:00 +0100

    conditional rendering

--- client/src/Pages/Profile/index.jsx
+++ client/src/Pages/Profile/index.jsx
@@ -18,21 +18,46 @@ let UserData = {
   yours: 6,
 };
 
+let options = ["Activities", "Bookmarks", "Your Events", "Reviews", "Manage"];
+
 function Profile() {
   const [userDetails, setUserDetails] = useState(UserData);
+  const [postBoardOption, setPostBoard] = useState("Activities");
 
+  function renderPostBoard(value) {
+    console.log("inside");
+    setPostBoard(value);
+  }
+
+  function renderPostBoar() {
+    if (postBoardOption == "Activities") {
+      return <PostBoard />;
+    } else if (postBoardOption == "Bookmarks") {
+      return <PostBoard />;
+    } else if (postBoardOption == "Your Events") {
+      return <PostBoard />;
+    } else if (postBoardOption == "Reviews") {
+      return <PostBoard />;
+    } else {
+      return <h1>Hello</h1>;
+    }
+  }
   return (
     <div className="section__block-Y-5">
       <Grid container spacing={2}>
         <Grid item xs={12}>
-          <Banner userInfo={userDetails} />
+          <Banner
+            userInfo={userDetails}
+            bannerOptions={options}
+            changePostBoard={renderPostBoard}
+          />
         </Grid>
         <Grid item xs={12} md={3}>
           <AboutUser userInfo={userDetails} />
           <ActionOptions />
         </Grid>
         <Grid item xs={12} md={9}>
-          <PostBoard />
+          {renderPostBoar()}
         </Grid>
       </Grid>
     </div>
