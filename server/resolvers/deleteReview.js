2da829deec7521d237f3584f69fc0197bfd19fc4
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-09 14:24:23 +0100

    delete review partially done

--- server/resolvers/deleteReview.js
+++ server/resolvers/deleteReview.js
@@ -1,1 +1,25 @@
+const { ApolloError, AuthenticationError } = require('apollo-server');
 
+const { Review, Event } = require('../models');
+
+const deleteReview = async (_, { reviewId }, { user }) => {
+  try {
+    if (user) {
+      const deleteReviewFromDatabase = await Review.findByIdAndDelete(reviewId);
+
+      //TODO: figure out logic for dlete
+      // await Event.findByIdAndDelete();
+
+      return deleteReviewFromDatabase;
+    } else {
+      throw new AuthenticationError(
+        'You must be logged in to delete a review.',
+      );
+    }
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete review | ${error.message}`);
+    throw new ApolloError('Failed to delete review');
+  }
+};
+s;
+module.exports = deleteReview;
