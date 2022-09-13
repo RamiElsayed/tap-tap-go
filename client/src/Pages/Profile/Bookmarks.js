fc9eb0e87baa86c8930f232c8a313c97685c0365
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-13 23:42:26 +0100

    refactor eventcard prop

--- client/src/Pages/Profile/Bookmarks.js
+++ client/src/Pages/Profile/Bookmarks.js
@@ -1,22 +1,23 @@
-import { Card, CardContent, Typography, Grid, Box } from "@mui/material";
-import React, { useState } from "react";
-import EventCard from "../../components/Cards/EventCard.js";
+import { Card, CardContent, Typography, Grid, Box } from '@mui/material';
+import React, { useState } from 'react';
+import EventCard from '../../components/Cards/EventCard.js';
 
 const cardData = {
-  title: "Salsa with Fabian Sarango",
-  price: "From £15 per person",
+  title: 'Salsa with Fabian Sarango',
+  price: 'From £15 per person',
   value: 4,
   nRatings: 254,
 };
 
 function Bookmarks({ bookmarkData }) {
+  console.log(bookmarkData);
   return (
-    <Card sx={{ width: { xs: "100%", md: "100%" } }}>
+    <Card sx={{ width: { xs: '100%', md: '100%' } }}>
       <CardContent
         sx={{
-          width: { xs: "99%", md: "100%" },
-          marginX: "auto",
-          paddingY: "2rem",
+          width: { xs: '99%', md: '100%' },
+          marginX: 'auto',
+          paddingY: '2rem',
         }}
       >
         <Grid
@@ -26,10 +27,10 @@ function Bookmarks({ bookmarkData }) {
           justifyContent="center"
           className="section__block-4"
         >
-          {bookmarkData.map((el) => {
+          {bookmarkData.map((el, i) => {
             return (
               <Grid item xs={11} sm={10} md={4} lg={3}>
-                <EventCard cardData={el} />
+                <EventCard {...el} key={i} />
               </Grid>
             );
           })}
