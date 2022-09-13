3bdee41a5a4a11285cde9a3d20d2988d6c15cc81
Sinh Fu San <ss12932@my.bristol.ac.uk>
2022-09-13 12:25:12 +0100

    populate user resolvers

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,12 +1,15 @@
-const { User } = require("../models");
+const { User } = require('../models');
 
 const user = async (_, { userId }) => {
   try {
-    const userFromDatabase = await User.findById(userId);
+    const userFromDatabase = await User.findById(userId)
+      .populate('bookmarks')
+      .populate('reviews')
+      .populate('events');
     return userFromDatabase;
   } catch (err) {
     console.log(`[ERROR]: Failed to get user | ${err.message}`);
-    throw new ApolloError("Failed to get user");
+    throw new ApolloError('Failed to get user');
   }
 };
 module.exports = user;
