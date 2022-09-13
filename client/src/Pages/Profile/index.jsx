d00b85ad2af9f673290655d372ba3e47255b685b
Rami Elsayed <ramielsayed123@outlook.com>
2022-09-13 15:08:03 +0100

    rendering user data for bookmarks

--- client/src/Pages/Profile/index.jsx
+++ client/src/Pages/Profile/index.jsx
@@ -1,36 +1,40 @@
 import { Grid } from "@mui/material";
-import React, { useState } from "react";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 import Banner from "./Banner";
 import AboutUser from "./AboutUser";
 import PostBoard from "./PostBoard";
 import Bookmarks from "./Bookmarks";
 import ActionOptions from "./ActionOptions";
 import ReviewForm from "../../components/ReviewForm/index";
+import { useQuery } from "@apollo/client";
+import { GET_PROFILEDATA, QUERY_ME } from "../../graphQL/queries";
+import areIntervalsOverlappingWithOptions from "date-fns/esm/fp/areIntervalsOverlappingWithOptions/index";
 
-let UserData = {
-  firstName: "Fabian",
-  lastName: "Sarango",
-  address: "3 barleycorn Drive",
-  username: "Fabian Sarango",
-  number: "075429718393",
-  email: "fasasa@gmail.com",
-  attended: 4,
-  upcoming: 5,
-  yours: 6,
-};
+// Use optional chaining to check if data exists and if it has a thoughts property. If not, return an empty array to use.
 
 let options = ["Activities", "Bookmarks", "Your Events", "Reviews", "Manage"];
 
 function Profile() {
-  const [userDetails, setUserDetails] = useState(UserData);
+  //const { userId: userParam } = useParams();
+  const userParam = "632071e89a8b7213cf9d0812";
+  const { loading, data } = useQuery(GET_PROFILEDATA, {
+    variables: { userId: userParam },
+  });
+  const userDetails = data?.user || [];
+
+  //const [userDetails, setUserDetails] = useState(UserData);
+  console.log(userDetails);
+
+  //const [userDetails, setUserDetails] = useState(UserData);
   const [postBoardOption, setPostBoard] = useState("Activities");
 
-  function renderPostBoard(value) {
+  function changeBoardOptions(value) {
     console.log("inside");
     setPostBoard(value);
   }
 
-  function renderPostBoar() {
+  function renderPostBoard() {
     if (postBoardOption == "Activities") {
       return <PostBoard />;
     } else if (postBoardOption == "Bookmarks") {
@@ -52,15 +56,15 @@ function Profile() {
           <Banner
             userInfo={userDetails}
             bannerOptions={options}
-            changePostBoard={renderPostBoard}
+            changePostBoard={changeBoardOptions}
           />
         </Grid>
         <Grid item xs={12} md={3}>
           <AboutUser userInfo={userDetails} />
-          <ActionOptions changePostBoard={renderPostBoard} />
+          <ActionOptions changePostBoard={changeBoardOptions} />
         </Grid>
         <Grid item xs={12} md={9}>
-          {renderPostBoar()}
+          {renderPostBoard()}
         </Grid>
       </Grid>
     </div>
