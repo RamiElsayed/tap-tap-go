fbeab7ea64db7ad80f986313c423a0dac7f378cd
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-09-16 18:26:53 +0100

    commented it out

--- client/src/components/bookmark/index.js
+++ client/src/components/bookmark/index.js
@@ -12,22 +12,22 @@ const style = {
   bgcolor: "background.paper",
 };
 
-const theme = createTheme({
-  typography: {
-    fontFamily: [
-      "-apple-system",
-      "BlinkMacSystemFont",
-      '"Segoe UI"',
-      "Roboto",
-      '"Helvetica Neue"',
-      "Arial",
-      "sans-serif",
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(","),
-  },
-});
+// const theme = createTheme({
+//   typography: {
+//     fontFamily: [
+//       "-apple-system",
+//       "BlinkMacSystemFont",
+//       '"Segoe UI"',
+//       "Roboto",
+//       '"Helvetica Neue"',
+//       "Arial",
+//       "sans-serif",
+//       '"Apple Color Emoji"',
+//       '"Segoe UI Emoji"',
+//       '"Segoe UI Symbol"',
+//     ].join(","),
+//   },
+// });
 
 function BookMark() {
   return (
