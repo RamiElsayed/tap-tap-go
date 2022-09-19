bd3f504e565dcfb9d88738cb81ce6f0df621143a
frsargua <frsargua@gmail.com>
2022-09-19 12:47:00 +0100

    Bug

--- server/resolvers/login.js
+++ server/resolvers/login.js
@@ -1,6 +1,6 @@
-const { AuthenticationError } = require('apollo-server');
-const { User } = require('../models');
-const { signToken } = require('../context/auth');
+const { AuthenticationError } = require("apollo-server");
+const { User } = require("../models");
+const { signToken } = require("../context/auth");
 
 const login = async (_, { input }) => {
   try {
@@ -8,17 +8,18 @@ const login = async (_, { input }) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      console.log('[ERROR]: Failed to login | Cannot find user');
-      throw new AuthenticationError('Failed to login');
+      console.log("[ERROR]: Failed to login | Cannot find user");
+      throw new AuthenticationError("Failed to login");
     }
 
     const correctPassword = await user.isCorrectPassword(password);
 
     if (!correctPassword) {
-      console.log('[ERROR]: Failed to login | Incorrect password');
-      throw new AuthenticationError('Failed to login');
+      console.log("[ERROR]: Failed to login | Incorrect password");
+      throw new AuthenticationError("Failed to login");
     }
-    console.log('You have successfully logged in');
+    console.log("You have successfully logged in");
+    console.log(user);
     return {
       token: signToken(user),
       user,
